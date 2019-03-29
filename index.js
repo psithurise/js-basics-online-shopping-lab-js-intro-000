3879f4f22e15a8b91b06eede3fddde33426a091c
Rose Stough <psithurise@programmer.net>
2019-03-29 22:18:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ function viewCart() {
   else if (cart.length == 1)
     return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
   else if(cart.length == 2)
-    return `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}, and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}`
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}`
   else {
     var arr = []
     for(let i = 0; i<cart.length-1; i++) {
-      arr.push(` ${Object.keys(cart[i])} at $${Object.values(cart[i])},`)
+      arr.push(` ${getCart()[i].itemName} at $${getCart()[i].itemPrice},`)
     }
-    return `In your cart, you have${arr} and ${Object.keys(cart[cart.length-1])} at $${Object.values(cart[cart.length-1])}.`
+    return `In your cart, you have${arr} and ${getCart()[cart.length-1].itemName} at $${getCart()[cart.length-1].itemPrice}.`
   }
 }
 
