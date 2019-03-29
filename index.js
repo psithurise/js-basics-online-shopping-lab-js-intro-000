86f90a3dabd86dcb908e9ef7f10f8d8881fd0d2f
Rose Stough <psithurise@programmer.net>
2019-03-29 23:30:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function viewCart() {
     for(let i = 1; i<cart.length-1; i++) {
       arr.push(` ${getCart()[i].itemName} at $${getCart()[i].itemPrice},`)
     }
-    return `In your cart, you have ${getCart()[0].itemName} at ${getCart()[0].itemPrice},${arr} and ${getCart()[cart.length-1].itemName} at $${getCart()[cart.length-1].itemPrice}.`
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice},${arr} and ${getCart()[cart.length-1].itemName} at $${getCart()[cart.length-1].itemPrice}.`
   }
 }
 
