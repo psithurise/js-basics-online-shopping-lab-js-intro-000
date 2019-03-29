4dfc943e0efcf106debd7bc90c0550238374cec8
Rose Stough <psithurise@programmer.net>
2019-03-29 23:57:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ function removeFromCart(item) {
   for(let i=0; i<cart.length; i++) {
     if(getCart()[i].itemName == item) {
       let toRemove = cart.indexOf(item)
-      return cart.splice(toRemove, 1)
+      cart.splice(toRemove, 1)
+      return cart
     }
   }
   return "That item is not in your cart."
